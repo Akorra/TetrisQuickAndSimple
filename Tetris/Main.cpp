346a65cebede51f5d9ca4efee7cab95cfc78ea30
Akorra <neverfearphilishere@gmail.com>
2021-06-08 00:18:00 +0100

    almost tetris

--- Tetris/Main.cpp
+++ Tetris/Main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <vector>
 #include <Windows.h>
 
 std::wstring tetromino[7];
@@ -22,9 +25,27 @@ int Rotate(int px, int py, int r)
 	return 0;
 }
 
-bool isLegalMove(int nTetromino, int nRotation, int nPosX, int nPosy)
+bool isLegalMove(int nTetromino, int nRotation, int nPosX, int nPosY)
 {
+	for (int px = 0; px < 4; px++)
+		for (int py = 0; py < 4; py++)
+		{
+			//Get index into piece
+			int pi = Rotate(px, py, nRotation);
 
+			//Get index into filed
+			int fi = (nPosY + py) * nFieldWidth + (nPosX + px);
+
+			if (nPosX + px >= 0 && nPosX + px < nFieldWidth)
+			{
+				if (nPosY + py >= 0 && nPosY + py < nFieldHeight)
+				{
+					if (tetromino[nTetromino][pi] == L'X' && pField[fi] != 0)
+						return false; //fail on first hit
+				}
+			}
+
+		}
 	return true;
 }
 
@@ -51,24 +72,124 @@ int main()
 	SetConsoleActiveScreenBuffer(hConsole);
 	DWORD dwBytesWritten = 0;
 
+	// Game Logic stuff
 	bool bGameOver = false;
+
+	int nCurrentPiece = rand() % 7;
+	int nCurrentRotation = 0;
+	int nCurrentX = nFieldWidth / 2;
+	int nCurrentY = 0;
+
+	bool bKey[4];
+	bool bRotateHold = false;
+
+	int nSpeed = 20;
+	int nSpeedCounter = 0;
+	bool bForceDown = false;
+
+	std::vector<int> vLine;
 
 	while (!bGameOver)
 	{
 		// GAME TIMING
+		std::this_thread::sleep_for(std::chrono::milliseconds(50)); //1 game tick
+		nSpeedCounter++;
+		bForceDown = (nSpeedCounter == nSpeed);
 		 
-		
 		// INPUT
-		
+		for (int k = 0; k < 4; k++)								// R   L   D Z
+			bKey[k] = (0x8000 & GetAsyncKeyState((unsigned char)("\x27\x25\x28Z"[k]))) != 0;
+
 		// GAME LOGIC
-		 
+		nCurrentX -= (bKey[1] && isLegalMove(nCurrentPiece, nCurrentRotation, nCurrentX - 1, nCurrentY)) ? 1 : 0; //LEFT
+		nCurrentX += (bKey[0] && isLegalMove(nCurrentPiece, nCurrentRotation, nCurrentX + 1, nCurrentY)) ? 1 : 0; //RIGHT
+		nCurrentY += (bKey[2] && isLegalMove(nCurrentPiece, nCurrentRotation, nCurrentX, nCurrentY + 1)) ? 1 : 0; //DOWN
+
+		if (bKey[3] )
+		{
+			nCurrentRotation += (!bRotateHold && isLegalMove(nCurrentPiece, nCurrentRotation + 1, nCurrentX, nCurrentY)) ? 1 : 0;
+			bRotateHold = true;
+		}
+		else
+		{
+			bRotateHold = false;
+		}
+
+		if (bForceDown)
+		{
+			if (isLegalMove(nCurrentPiece, nCurrentRotation, nCurrentX, nCurrentY + 1))
+				nCurrentY++; // It can, so do it
+			else
+			{
+				//Lock current piece in field
+				// Draw Current Piece
+				for (int px = 0; px < 4; px++)
+					for (int py = 0; py < 4; py++)
+						if (tetromino[nCurrentPiece][Rotate(px, py, nCurrentRotation)] == L'X')
+							pField[(nCurrentY + py) * nFieldWidth + (nCurrentX + px)] = nCurrentPiece + 1; 
+
+				// Check have we vcreated horizontal lines
+				for (int py = 0; py < 4; py++)
+					if (nCurrentY + py < nFieldHeight - 1)
+					{
+						bool bLine = true;
+						for (int px = 1; px < nFieldWidth - 1; px++)
+							bLine &= (pField[(nCurrentY + py) * nFieldWidth + px]) != 0;
+
+						if (bLine)
+						{
+							//Remove Line set to =
+							for (int px = 1; px < nFieldWidth - 1; px++)
+								pField[(nCurrentY + py) * nFieldWidth + px] = 8; // 8 -> =
+							
+							vLine.push_back(nCurrentY + py);
+						}
+					}
+
+				///choose next piece
+				nCurrentX = nFieldWidth / 2;
+				nCurrentY = 0;
+				nCurrentRotation = 0;
+				nCurrentPiece = rand() % 7;
+
+				// if piece does not fit
+				bGameOver = !isLegalMove(nCurrentPiece, nCurrentRotation, nCurrentX, nCurrentY);
+			}
+
+			nSpeedCounter = 0;
+		}
+
 		// RENDER OUTPUT
 
 		// Draw Field
 		for (int x = 0; x < nFieldWidth; x++)
 			for (int y = 0; y < nFieldHeight; y++)
 				screen[(y+2)*nScreenWidth + (x+2)] = L" ABCDEFG=#"[pField[y*nFieldWidth+x]];
+
+		// Draw Current Piece
+		for (int px = 0; px < 4; px++)
+			for (int py = 0; py < 4; py++)
+				if (tetromino[nCurrentPiece][Rotate(px, py, nCurrentRotation)] == L'X')
+					screen[(nCurrentY + py + 2) * nScreenWidth + (nCurrentX + px + 2)] = nCurrentPiece + 65; //+ 65 for ascii letters
 		
+
+		if (!vLine.empty())
+		{
+			//Display Frame
+			WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+			std::this_thread::sleep_for(std::chrono::milliseconds(400));
+
+			for (auto& v : vLine)
+				for (int px = 1; px < nFieldWidth - 1; px++)
+				{
+					for (int py = v; py > 0; py--)
+						pField[py * nFieldWidth + px] = pField[(py - 1) * nFieldWidth + px];
+					pField[px] = 0;
+				}
+
+			vLine.clear();
+		}
+
 		// Display Frame
 		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
 	}
